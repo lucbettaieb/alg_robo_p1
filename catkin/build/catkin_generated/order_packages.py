36c40d119796efb93c3a3a18dda91b8e357561a1
Luc Bettaieb <luc.bettaieb@gmail.com>
2014-10-20 20:31:52 -0400

    Initial Commit

---
+++ catkin/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/luc/ros_workspace/alg_robo_p1/catkin/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/opt/ros/hydro".split(';') if "/opt/ros/hydro" != "" else []
