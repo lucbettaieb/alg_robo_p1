49e3ba5989da83d91519356cea2d6de74a2c8a8c
Luc Bettaieb <luc@ubuntu.(none)>
2014-10-23 00:19:38 -0400

    Added code to noisey.  Currently using std::normal_distribution.  This seems like a good choice.  Need to test.  Currently passing in the subscribed signal data as the parameter for the distrubtion operator.  This should give a value maybe around the number?  My understanding may be off.  I may have to dynamically update the mean of the distribition and use it as a base for where the noise is going to be coming from.  This approach would make sense because then the generated noise added to the signal would be gaussian for sure.  All in all, need to keep playing.

---
+++ catkin/src/noisey/src/noisey.cpp
@@ -0,0 +1,40 @@
+/*
+* Noisey
+* A ROS Node to subscribe to a stream of simulated sensor data, add Gaussian noise to it,
+* and republish it for probabilisitc-based filtering.
+*
+* Luc A. Bettaieb
+* bettaieb@case.edu
+*
+* 22.10.14
+*/
+
+#include <ros/ros.h>
+#include <std_msgs>
+#include <random>
+
+ros::Publisher pub_;
+ros::Subscriber sub_;
+
+//Random number generator and normal distribution (by default N(0,1))
+float mu = 0;
+float sigma = 1;
+
+//std::default_random_engine generator;
+std::normal_distribution<float> distribution(mu, sigma);
+
+//Signal to noise ratio
+//Will act as a constant to define how much noise is delivered to the signal
+float SNR = 0.5;			
+
+int main(int argc, char** argv){
+	//Standard ROS Initialization Stuff
+	ros::init(argc, argc, "noisey")
+
+}
+
+//Callback function for when node recieves a signal value (should be always)
+float addNoise(const std_msgs::Float64 &signal){
+	return distribution(signal.data); //simply trying to use the input to the dist. as the sigval
+}
+
