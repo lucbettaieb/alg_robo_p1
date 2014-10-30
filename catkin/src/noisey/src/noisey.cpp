3e47be966ed8054f388eeb714c14018aeb62dccf
Luc Bettaieb <luc.bettaieb@gmail.com>
2014-10-30 15:10:34 -0400

    Changed up how adding noise is handled.  Now uses a dedicated function rather than <random>.  Need to still handle a few bugs.

--- catkin/src/noisey/src/noisey.cpp
+++ catkin/src/noisey/src/noisey.cpp
@@ -14,9 +14,10 @@
 #include <sensor_msgs/LaserScan.h>
 #include <ctime>
 #include <stdlib.h>
-#include <random> 
 #include <stdio.h>
+#include <math.h>
 #define countof( array ) ( sizeof( array )/sizeof( array[0] ) )
+#define TWO_PI 6.2831853071795864769252866
 ros::Publisher pub_;
 ros::Subscriber sub_;
 
@@ -25,12 +26,32 @@ float mu = 0;
 float sigma = 1;
 
 //std::default_random_engine generator;
-std::normal_distribution<std_msgs::Float32> distribution(0.0, 1.0);
+//std::normal_distribution<std_msgs::Float32> distribution(0.0, 1.0);
 
 //Signal to noise ratio
 //Will act as a constant to define how much noise is delivered to the signal
 float SNR = 0.5;			
  
+//Box Nuller transform for adding normally distributed noise to the signal (thanks wikipedia)
+double addGaussianNoise(const double &variance){
+	static bool haveSpare = false;
+	static double rand1, rand2;
+
+	if(haveSpare){
+		haveSpare = false;
+		return sqrt(variance * rand1) * sin(rand2);
+	}
+
+	haveSpare = true;
+
+	rand1 = std::rand() / ((double) RAND_MAX);
+	if(rand1 < 1e-100) rand1 = 1e-100;
+	rand1 = -2*log(rand1);
+	rand2 = ((rand() / ((double) RAND_MAX)) * TWO_PI);
+
+	return sqrt(variance * rand1) * cos(rand2);
+}
+
 //Callback function for when node recieves a signal value (should be forever)
 void addNoiseLaser(const sensor_msgs::LaserScan &scan){ //need to get sensor_msgs/LaserScan, iterate through ranges[] and add noise then publish new laserscan
 	sensor_msgs::LaserScan noiseyScan;
@@ -40,7 +61,12 @@ void addNoiseLaser(const sensor_msgs::LaserScan &scan){ //need to get sensor_msg
 
 	for(int i = 0; i < countof(scan.intensities); i++){ //TODO: Check if length function of float32[] is real
 		if( randomNumber >= (1-SNR) ){ //should converge on SNR over time for all the signal noise n stuff.  I could test this.  See if LaserScan.intensity[i] / NoiseyScan.intensity[i] == 1 or not, then divide the nots by the 1's and that should equal SNR after a while 
-			noiseyScan.intensities[i] = distribution(scan.intensities[i]);
+			if((randomNumber*10)%2 == 0){ //Using the same number to see if its even or odd (50/50 chance, right?)
+				noiseyScan.intensities[i] += (float)addGaussianNoise(.01);
+			} else{
+				noiseyScan.intensities[i] -= (float)addGaussianNoise(.01);
+			}
+			
 		} else{
 			noiseyScan.intensities[i] = scan.intensities[i];
 		}
@@ -55,6 +81,10 @@ int main(int argc, char** argv){
 	ros::NodeHandle nh;
 	//Init random number generator with time seed
 
-	sub_ = nh.subscribe("/robot0/LaserScan", 1, addNoiseLaser); //make sure its robot0...  This may need to be fixed
+	while(ros::ok()){
+		sub_ = nh.subscribe("/robot0/LaserScan", 1, addNoiseLaser); //make sure its robot0...  This may need to be fixed
+		ros::spinOnce();
+	}
+	
 
 }
