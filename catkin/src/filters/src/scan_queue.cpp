de72dbba8a3ed465d54529957fa802072200e6e8
lucbettaieb <luc.bettaieb@gmail.com>
2014-11-14 02:30:47 -0500

    scan_queue bugs tackled for the most part.  progress may now progress.

--- catkin/src/filters/src/scan_queue.cpp
+++ catkin/src/filters/src/scan_queue.cpp
@@ -24,6 +24,8 @@ bool newData_ = true;			//Global boolean to determine whether or not to get a ne
 
 void parseFile(){
 	algp1_msgs::PoseScan poseScan;
+	std::vector<std::string> list;
+	std::string line;
 
 	std::ifstream fin;
 	fin.open("rangemap_simple.txt");
@@ -31,53 +33,70 @@ void parseFile(){
 		ROS_INFO("rangemap not found");
 		ros::shutdown();
 	}
-	std::vector<std::string> list;
+	
 	list.clear();
-	std::string line;
+	
 	line.resize(6000);
 	ROS_INFO("Copying file to vector");
 
 	for(int i = 0; i < 1960; i++){
-		if(std::getline(fin, line)){
-			list.push_back(line);
+		if(std::getline(fin, line)){ 	//If there is a line to get
+			list.push_back(line);		//Push it to the vector
 		}
 	}
+
 	ROS_INFO("Done copying file to vector.");
-	for(int i = 0; i < 1960; i+= 10){
-		int com = list.at(i).find(",");
+
+	for(int i = 0; i < 1960; i+= 10){ 	//For every 10th element ([x,y])
+		int com = list.at(i).find(",");	//Find the comma
 		
-		std::string ex = list.at(i).substr(1,com-1);
-		std::string ey = list.at(i).substr(com+1);
-		ey.resize(ey.size()-1);
+		std::string ex = list.at(i).substr(1,com-1); 	//Create a string that will represent X
+		std::string ey = list.at(i).substr(com+1);	 	//Create a strong that will represent Y] (for some reason the bracket stays)
+		ey.resize(ey.size()-1);						 	//Remove the bracket
 
-		float x = (float)std::atoi(ex.c_str());
-		float y = (float)std::atoi(ey.c_str());
+		float x = (float)std::atoi(ex.c_str());	//Make a float from the string for X
+		float y = (float)std::atoi(ey.c_str());	//And Y
 
-		poseScan.pose2d.x = x;
-		poseScan.pose2d.y = y;
-		//std::cout << x << " " << y <<std::endl;
-		for(int a = 0; a < 1960; a++){
-			if((a % 10)!= 0){
+		poseScan.pose2d.x = x;	//Assign X to X in pose2d
+		poseScan.pose2d.y = y;	//Assign Y to Y in pose2d
+
+		for(int a = 0; a < 1960; a++){ 	//Loop through the entire vector again
+			if((a % 10)!= 0){			//And if its not one of the lines where there is a []
+			
+				std::vector<std::string> rangeVector; //DEFINE VECTOR TO HOLD RANGES 
+
 				std::string ranges;
 				//ranges.resize(6000);
 				ranges = list.at(a);
 				//std::cout << ranges.size() << std::endl;
 
-				if(!(ranges.size() < 2)){
-					ROS_INFO("Hello");
-					ranges = list.at(a).substr(1);
-					ranges.resize(ranges.size()-1);
-					for(int b = 0; b < ranges.size(); b++){
-						//
-					}
-					std::cout << ranges<< std::endl;
-				}
+					if(ranges.size() > 2){ //If the line is not blank
+					
+						ranges = list.at(a).substr(1);
+						ranges.resize(ranges.size()-1);
+				
+						std::string tmp = ranges.substr(0);					//copy over ranges
+						int delim = tmp.find(",");
+						
+						ranges = ranges.substr(delim+1); //ranges is now one element less
 
+						if(delim > 1){
+							tmp.resize(delim);
+
+							std::cout << a << " - "<< tmp << std::endl;
+						}
 				
 				
-			}
+
+
+					
+				//std::cout << ranges<< std::endl;
+			}				
 		}
 	}
+	}
+		//std::cout << x << " " << y <<std::endl; //Print out X and Y
+	
 }
 
 int main(int argc, char** argv){
